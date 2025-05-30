f5765622ab2093fe9696aa7f5f6bc38745e61d8b
chrisrobles <christianrobles702@gmail.com>
2025-05-30 04:22:17 -0700

    updated python cheatsheet

--- Python 3/0-cheatsheet.py
+++ Python 3/0-cheatsheet.py
@@ -21,6 +21,12 @@ Print
 print("hello", end="!")
 
 """
+User Input
+"""
+
+input("Enter your name")
+
+"""
 Variables
 """
 
@@ -205,26 +211,32 @@ Tuples
 tup = (1,2,3)
 4,5,6 # => (4,5,6)
 
-"""
-Iterable Helper Functions
-"""
-
-# Apply function to each value
-map(max, [1,2,3], [4,2,1])           # => [4,2,1]
-
-# Filter out values
-filter(lambda x: x > 5, [3,4,5,6,7] ) # => [6,7]
+tup[0] = 1 # Raises TypeError: 'tuple' object does not support item assignment
 
 """
 Dict
 """
 
-# {key: value}
+# {key: value} iterable
 filled_dict = {"one": 1, "two": 2, "three": 3}
 
 # Key must be immutable type
 invalid_dict = {[1,2,3]: "123"} # Raises TypeError: unhashable type
 valid_dict = {(1,2,3): "123"}
+
+# Get by key
+filled_dict["four"]        # Raises KeyError
+filled_dict.get("four")    # => None
+filled_dict.get("four", "default")    # => 'default value'
+
+# Set key and value
+filled_dict["four"] = 4
+
+# Check for existing key
+"four" in filled_dict      # => False
+
+# Set if key not defined
+filled_dict.setdefault("four", 4)
 
 # Get all keys
 list(valid_dict.keys())   # Python <3.7 = random order, Python 3.7+ = in order
@@ -232,22 +244,9 @@ list(valid_dict.keys())   # Python <3.7 = random order, Python 3.7+ = in order
 # Get all values
 list(valid_dict.values()) # Python <3.7 = random order, Python 3.7+ = in order
 
-# Check for existing key
-"four" in filled_dict      # => False
-
-# Get by key
-filled_dict["four"]        # Raises KeyError
-filled_dict.get("four")    # => None
-filled_dict.get("four", "default")    # => 'default value'
-
-# Set if key not defined
-filled_dict.setdefault("four", 4)
-
-# Set key and value
-filled_dict["four"] = 4
-
-# Remove key
-del filled_dict["four"]
+# Remove key and value
+filled_dict.pop("four")    # Raises KeyError if not found
+del filled_dict["four"]    # returns value, Raises KeyError if not found
 
 # Dict Comprehension / One line loop
 # {*return* *loop* *condition*}
@@ -258,46 +257,73 @@ Sets
 """
 
 # Unique tuple
-my_set = {1,1,2,3} # => {1,2,3}
+my_set = {1,1,2,3}     # => {1,2,3}
 
-# Values have to immutables
-invalid_set = {[1], 1}
-
-# Add
-my_set.add(4)
+# Values have to be immutables
+invalid_set = {[1], 1} # Raises TypeError: unhashable type: 'list'
 
 # Intersection (overlap)
-my_set & {3,4,5}     # => {3,4}
+my_set & {3,4,5}       # => {3,4}
 
 # Union
-my_set | {3,4,5}     # => {1,2,3,4,5}
+my_set | {3,4,5}       # => {1,2,3,4,5}
 
 # Difference (only look at left)
-my_set - {4,5,6}     # => {1,2,3}
+my_set - {4,5,6}       # => {1,2,3}
 
 # Symmetric Difference (look at both)
-my_set ^ {2,3,4,5,6} # => {1,6}
+my_set ^ {2,3,4,5,6}   # => {1,6}
 
 # Superset (child)
-{1,2} >= {1,2,3}    # => False
-{1,2} <= {1,2,3}    # => True
+# Check if all values in left are in right
+{1,2} >= {1,2,3}       # => False
+# Check if all values in right are in left
+{1,2} <= {1,2,3}       # => True
 
 # Check if exists
-2 in my_set          # => True
+2 in my_set            # => True
+
+"""
+Iterable Helper Functions
+"""
+
+# Apply function to each value
+map(max, [1,2,3], [4,2,1])           # => [4,2,1]
+
+# Filter out values
+filter(lambda x: x > 5, [3,4,5,6,7] ) # => [6,7]
 
 # Copy (by value)
 my_copy = my_set.copy()
-my_copy is my_set     # => False
+my_copy is my_set      # => False
+
+"""
+Exceptions
+"""
+
+try:
+    raise IndexError("uh oh index error")
+except IndexError as e:
+    pass
+except (TypeError, NameError):
+    pass
+else:
+    print("All good no exceptions")
+finally:
+    print("Clean up resources here!")
+    
+# Instead of try/finally for resource cleanup use a with statement
+
 
 """
 Functions
 """
+
 def add(x,y):
     return x + y, x, y  # return multiple values as tuple
 
 # Positional Arguments
 add(5, 2)
-
 # Keyword Arguments
 add(y=5, x=2)
 add({"y":5, "x":2})     # the same
@@ -305,24 +331,22 @@ add({"y":5, "x":2})     # the same
 # Catch all Positional Arguments
 def all_positional(*args):
     return args
-
 # Catch all Keyword Arguments
 def all_keyword(**kwargs):
     return kwargs
-
-# Both catch all
+# Catch both
 def my_func(*args, **kwargs):
     pass
 
-# First class functions
-# (functions are treated like variables)
-# functions can be passed as an argument, returned, assigned to another variable
+# First class functions (functions are treated like variables)
 def create_adder(x):
     def adder(y):
         return x + y
     return adder
 add_10 = create_adder(10)
-add_10(3)
+add_10(3) # => 13
+add_5 = create_adder(5)
+add_5(3)  # => 8
 
 # Refer to variables in the parent function
 def create_avg():
@@ -335,47 +359,27 @@ def create_avg():
         return total/count
     return avg
 
-# Use Once functions / Anonymous Functions
+# Lambda / Anonymous Functions (Use once)
 (lambda x: x > 2)(3) # => True
 
-"""
-Decorators
-"""
-
-# Wrappers
-def log_function(func):
-    def wrapper(*args, **kwargs):
-        print("Entering function", func.__name__)
-        result = func(*args, **kwargs)
-        print("Exiting function", func.__name__)
-        return result
-    return wrapper
-
-@log_function               # equivalent:
-def my_function(x,y):       # def my_function(x,y):
-    return x+y              #   return x+y
-                            # my_function = log_function(my_function)
-
-# Preserve function details of decorated functions
-from functools import wraps
-
-def log_function(func):
-    @wraps(func) # docstring, function name, arguments list, etc. are copied to the wrapper
-    def wrapper(*args, **kwargs):
-        print("Entering", func.__name__)
-        result = func(*args, **kwargs)
-        print("Exiting", func.__name__)
-        return result
-    return wrapper
-
-@log_function
-def my_function(x,y):
-    return x+y
-my_function(1,2)
-my_function.__name__             # => 'my_function'
+def my_function(a, b):
+    return a + b
+# Function metadata
+my_function.__doc__          # => 'This is a docstring for my_function.'
+my_function.__module__       # => '__main__'
+my_function.__defaults__     # => None (no default arguments)
+my_function.__annotations__  # => {} (no type annotations)
+my_function.__globals__      # => {'__name__': '__main__', ...} (global variables)
+my_function.__dict__         # => {'__module__': '__main__', ...} (function attributes)
+# Function attributes
+my_function.__name__        # => 'my_function'
+my_function.__qualname__    # => 'my_function' (qualified name)
+my_function.__doc__         # => 'This is a docstring for my_function.'
+my_function.__module__      # => '__main__' (module name)
+my_function.__defaults__    # => None (no default arguments)
+my_function.__annotations__ # => {} (no type annotations)
+my_function.__globals__     # => {'__name__': '__main__', ...} (global variables)
 my_function.__code__.co_argcount # => 2
-    
-    
 
 """
 Class
@@ -388,22 +392,22 @@ class Human:
         self.name = name        # instance attribute
         self._age = 0           # instance attribute (_ denotes to use internally)
         
-    def sing(self):             # instance method (self is instance object)
+    def sing(self):             # instance method (self is an object of the instance and is passed automatically)
         return "{} sings hallelujah".format(name=self.name)
     
     @classmethod
     def get_species(cls):       # class method (cls is class object)
-        return cls.species
+        return cls.species      # class attributes can be accessed through cls
     
     @staticmethod               # static method (no class or instance object)
-    def grunt():
+    def grunt():                # static methods do not have access to instance or class attributes
         return "*grunts*"
     
     @property                   # getter method (trivial getters and setters not required in python)
     def age(self):
         return self._age
     
-    @age.setter                 # allows the property to be set
+    @age.setter                 # allows property to be set
     def age(self, age):
         self._age = age
     
@@ -411,36 +415,37 @@ class Human:
     def age(self):
         del self._age
         
-i = Human(name="Ian")
-i.say("hi")
-Human.species
-Human.grunt()
-i.grunt()
+me = Human(name="Chris")
+me.sing()
+Human.species = "H. sapiens sapiens"  # change class attribute
+Human.grunt() # => '*grunts*'
+i.grunt()     # => '*grunts*'
 del i.age
 
 """
 Inheritance
 """
 
-# Allows methods and variables to be inherited from their parent class
-class Superhero(Human):
-    species = "Superhuman"      # override parents' attributes
+class Superhero(Human):         # Inherit methods and variables from parent class
+    species = "Superhuman"      # override parent's attributes
     
-    def _init_(self, name, superpowers=["super strength", "bulletproofing"]):
+    def _init_(self, name, superpowers=None): 
         self.fictional = True
+        if superpowers is None: # default argument assign to none to avoid the same list being shared across instances
+            superpowers = ["super strength", "bulletproof"]
         self.superpowers = superpowers
-        super().__init__(name)  # inherits age attribute
+        super().__init__(name)  # call the parent's constructor (inherits age attribute)
     
-    def sing(self):             # override parents'
+    def sing(self):             # override parent's method
         return "dun , dun, dun!"
 
 # Check if instance of
 sup = Superhero(name="Spider-Man")
-if isinstance(sup, Human):
+if isinstance(sup, Human):      # check if instance of Human or subclass
     print("I am a human")
-if type(sup) is Superhero:
+if type(sup) is Superhero:      # check if instance of Superhero (not subclass)
     print("I am a superhero")
-    
+
 # Get method resolution order
 Superhero.__mro__ # <class '__main__.Superhero'>
                   # <class 'human.Human'>
@@ -459,13 +464,13 @@ class Bat:
         return msg
 class Batman(Superhero, Bat):
     def __init(self, *args, **kwargs):
-        # Instead of
-        # super(Batman, self).__init__(*args, **kwargs)
+        # Instead of super(Batman, self).__init__(*args, **kwargs)
         Superhero.__init__(self, "anonymous", movie=True, superpowers=["Wealthy"], *args, **kwargs)
         Bat.__init__(self, *args, can_fly=False, **kwargs)
         self.name = "Sad Affleck"
     def sing(self):
         return "nananana batman!"
+
 """
 Unpack / Pack
 """
@@ -490,25 +495,43 @@ Iterable
 
 # An object that can be treated as a sequence
 # More technically, an object that can create an iterator
+# Iterable is an object that implements __iter__() method
 
 # ex:
+# Range
 range(5)
-my_iterable = {"one": 1, "two": 2}.keys()
-list(my_iterable) # => ["one", "two"]
+# Dictionary
+my_iterable = {"one": 1, "two": 2}.items()
+# String
+my_iterable = "one two"
+# List
+my_iterable = ["one", "two"]
+# Set
+my_iterable = {"one", "two"}
+# Tuple
+my_iterable = ("one", "two")
+
 # Loop over iterable
 for i in my_iterable:
     print(i)      # 'one' 'two'
-    
-# No indexing
-my_iterable[0]    # Raises TypeError
 
-# Create iterator
-my_iterator = iter(my_iterable)
+# No indexing or slicing
+# No next() method (which is used by iterators)
+# If required, convert to list
+my_list = list(my_iterable) # => ['one', 'two']
 
 """
 Iterator
 """
+
 # Object that remembers the state as it traverses
+# Implements __iter__() and __next__() methods
+
+# An iterable can be converted to an iterator using iter()
+my_iterable = ["one", "two"]
+my_iterator = iter(my_iterable) # => <list_iterator object at 0x...>
+
+# Use next() to get the next value
 next(my_iterator) # => "one"
 next(my_iterator) # => "two"
 next(my_iterator) # Raises StopIteration
@@ -517,46 +540,26 @@ list(my_iterator) # => []
 # Loop over iterator
 for i in iter(my_iterable):
     print(i)      # 'one' 'two'
-    
-
-"""
-Exceptions
-"""
-
-try:
-    raise IndexError("uh oh index error")
-except IndexError as e:
-    pass
-except (TypeError, NameError):
-    pass
-else:
-    print("All good no exceptions")
-finally:
-    print("Clean up resources here!")
-    
-# Instead of try/finally for resource cleanup use a with statement
 
 """
 Files
 """
 
 # Open File
-with open("file.txt") as f:
+with open("file.txt") as f:     # with is used to ensure file is closed after use)
     # Loop each line
     for line in f:
         print(line)
 
 # Read File
 with open("file.txt", "r") as f:
-    print(f.read())
+    print(f.read())             # => read entire file as a string
 
 # Write file
 with open("file.txt", "w") as f:
     f.write("baba booey")
-    
-# Format as JSON
-import json
-with open("file.txt", "w") as f:
+    # Format as JSON
+    import json
     f.write(json.dumps({"a": 1, "b": 2}))
 
 # Load from JSON
@@ -565,22 +568,13 @@ with open("file.txt", "r") as f:
     print(json.load(f))
 
 """
-User Interaction
-"""
-
-input("Enter your name")
-
-"""
 Modules / 3rd Party Libraries
 """
-
-# Import
-import math
-math.sqrt(16)
 
 # Import local math.py
 # Local files in the same directory have priority
 import math
+math.sqrt(16)
 
 # Import specific functions
 from math import ceil, floor
@@ -601,23 +595,64 @@ import math
 dir(math)
 
 """
+Decorators
+"""
+
+# Wrappers
+def my_wrapper(func):
+    def wrapper(*args, **kwargs):
+        print("Entering function", func.__name__)
+        result = func(*args, **kwargs)
+        print("Exiting function", func.__name__)
+        return result
+    return wrapper
+
+@my_wrapper               
+def my_function(x,y):       
+    return x+y              
+
+# equivalent:
+# def my_function(x,y):
+#   return x+y
+# my_function = log_function(my_function)
+
+# Preserve function details of decorated functions
+from functools import wraps
+
+def log_function(func):
+    @wraps(func) # preserve metadata (docstring, function name, arguments list, etc.) are copied to the wrapper
+    def wrapper(*args, **kwargs):
+        # Do logging or other pre-processing
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
+
+@log_function
+def my_function(x,y):
+    return x+y
+my_function(1,2)
+
+"""
 Generators
 """
 
-# Memory-efficient 
-# (only load data needed to process the next value)
-def double_numbers(iterable):
-    for i in iterable:
-        yield i + i
+# Functions that return an iterator
+# Use yield to return values one at a time
+# Memory-efficient
+def count_up_to(n):
+    count = 1
+    while count <= n:
+        yield count
+        count += 1
         
 # range is a generator
-for i in double_numbers(range(1,900000000)):
+for i in count_up_to(range(1,900000000)):
     print(i)
     if i >= 30:
         break
     
-# generator comprehension
+# generator expression
 values = (-x for x in [1,2,3,4,5]) # => <generator object...
 for x in values:
     print(x)
-my_list = list(values)
+my_list = list(values) # => [-1, -2, -3, -4, -5]
--- Python 3/test.py
+++ Python 3/test.py
@@ -1,7 +1,5 @@
-def create_adder(x):
-    def adder(y):
-        print(x)
-        print(y)
-    return adder
-add_10 = create_adder(10)
-add_10(3)
+with open("file.txt", "w") as f:
+    f.write("baba booey")
+    # Format as JSON
+    import json
+    f.write(json.dumps({"a": 1, "b": 2}))
